0c228313de403e74b2f67a99d48363c01732b2d7
Arvind Rana <arvindrana6969@gmail.com>
2024-07-16 17:40:28 +0545

    button

---
+++ main.js
@@ -0,0 +1,3 @@
+function sendhelp() {
+    document.getElementById("dancing").innerHTML = "<marquee><img src='https://media4.giphy.com/media/Yoo0q5q0tg4dOgIgX5/giphy.gif?cid=6c09b95254a92a9ryorv5r4wx9tzx9z29kd09fhwhwo0rs42&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s' /></marquee>"
+}
